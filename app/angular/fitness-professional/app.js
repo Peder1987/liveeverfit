9f33a069222b205ff8ab8b72b689d4be12b28bdd
Miguel <miguel@miguel-Inspiron.(none)>
2014-05-06 17:50:27 -0500

    buttons

--- app/angular/fitness-professional/app.js
+++ app/angular/fitness-professional/app.js
@@ -1,11 +1,3 @@
-// This is really important
-// All one has to do to make a new app is clone this app
-// Rename the DIR
-// rename the demoCtrl just like DIRCtrl
-//change the name of demoCtrl in the index.html
-// and make a new state
-// Last put it in the nav bar 
-
 'use strict';
 
 define(['app'], function(app) {
@@ -17,57 +9,22 @@ define(['app'], function(app) {
             var professionalCollection =  $resource("http://:url/users/professionals/",{
                 url: $scope.restURL
             });
-            var professionalResource = $resource("http://:url/users/professionals/:id/",{
-                url: $scope.restURL,
-                id:'@id'
-            },{update: { method: 'PUT' }});
 
             $scope.professionals = professionalCollection.get(function() {},$scope.checkTokenError);
+            console.log($scope.professionals);
 
-            $scope.save = function(professional){
-                professionalResource.update({id:professional.id},professional)
-            }
-
-            $scope.add = function() {
-                professionalCollection.save($scope.newProfessional, function() {},
-                    function(error) {
-                    $scope.message = error.data;
-                    $scope.checkTokenError();
-                });
-            }
-
+            $scope.filter = function () {
+                console.log($scope.hello);
+            };
 
     }]);
 
-    app.register.controller("tagCtrl",["$scope","$resource","rest","tokenError",
-        function($scope,$resource,tokenError){
 
-            var professionalCollection =  $resource("http://:url/users/professionals/",{
-                url: $scope.restURL
-            });
-            var professionalResource = $resource("http://:url/users/professionals/:id/",{
-                url: $scope.restURL,
-                id:'@id'
-            },{update: { method: 'PUT' }});
-
-            $scope.professionals = professionalCollection.get(function() {},$scope.checkTokenError);
-
-            $scope.save = function(professional){
-                professionalResource.update({id:professional.id},professional)
-            }
-
-            $scope.add = function() {
-                professionalCollection.save($scope.newProfessional, function() {},
-                    function(error) {
-                    $scope.message = error.data;
-                    $scope.checkTokenError();
-                });
-            }
-
+    app.register.controller("tagCtrl",["$scope",
+        function($scope){
 
     }]);
-    return app;
 
-    
 
+    return app;    
 });
