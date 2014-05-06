c450a034d78077d4554be4a1c25371d10109f873
Miguel <miguel@miguel-Inspiron.(none)>
2014-05-06 18:35:36 -0500

    working on filtering

--- app/angular/fitness-professional/app.js
+++ app/angular/fitness-professional/app.js
@@ -6,18 +6,60 @@ define(['app'], function(app) {
     app.register.controller("fitness-professionalCtrl",["$scope","$resource","rest","tokenError",
         function($scope,$resource,tokenError){
 
-            $scope.hello = '';
+            $scope.Trainers = false;
+            $scope.Nutritionists = false;
+            $scope.pLocation = '';
+            $scope.Male = false;
+            $scope.Female = false;
+            $scope.pAcceptingClients = '';
+            $scope.pSpecialties = '';
 
             var professionalCollection =  $resource("http://:url/users/professionals/",{
                 url: $scope.restURL
             });
 
             $scope.professionals = professionalCollection.get(function() {},$scope.checkTokenError);
-            console.log($scope.professionals);
 
-            $scope.filter = function () {
-                console.log($scope.hello);
+            $scope.changeTrainers = function () {
+                if ($scope.Trainers == false){
+                    $scope.Trainers = true;
+                }
+                else{
+                    $scope.Trainers = false;
+                }
+                $scope.filter();
             };
+            $scope.changeNutritionists = function () {
+                if ($scope.Nutritionists == false){
+                    $scope.Nutritionists = true;
+                }
+                else{
+                    $scope.Nutritionists = false;
+                }
+                $scope.filter();
+            };
+            $scope.changeMale = function () {
+                if ($scope.Male == false){
+                    $scope.Male = true;
+                }
+                else{
+                    $scope.Male = false;
+                }
+                $scope.filter();
+            };
+            $scope.changeFemale = function () {
+                if ($scope.Female == false){
+                    $scope.Female = true;
+                }
+                else{
+                    $scope.Female = false;
+                }
+                $scope.filter();
+            };
+            $scope.filter = function () {
+                console.log('Working');
+            };
+
 
     }]);
 
