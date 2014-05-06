8f1ca3e9f28714d24d356adceee4944bf60fe4de
IsaaX <isaax@Bombero.(none)>
2014-05-06 18:02:26 -0500

    fixed syntax error

--- app/angular/workouts/app.js
+++ app/angular/workouts/app.js
@@ -10,7 +10,32 @@ define(['app'], function (app) {
     ]);
 
 
+    app.register.controller("workoutTagCtrl",["$scope","$resource","rest","tokenError",
+    function($scope,$resource,tokenError){
+
+        var tagCollection =  $resource("http://:url/tags/",{
+            url: $scope.restURL
+        });
+        var tagResource = $resource("http://:url/tags/:id/",{
+            url: $scope.restURL,
+            id:'@id'
+        },{update: { method: 'PUT' }});
+
+        $scope.tags = tagCollection.get(function() {},$scope.checkTokenError);
+
+        $scope.save = function(professional){
+            tagResource.update({id:tag.id},tag)
+        }
+
+        $scope.add = function() {
+            tagCollection.save($scope.newProfessional, function() {},
+                function(error) {
+                $scope.message = error.data;
+                $scope.checkTokenError();
+            });
+        }
 
 
+	}]);
 
 });
