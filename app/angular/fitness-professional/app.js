99cc2dce934dc6311cca10703ca630d8213481af
Nicolas Gonzalez <nickgzzjr@hotmail.com>
2014-05-12 11:44:39 -0500

    Styling.

--- app/angular/fitness-professional/app.js
+++ app/angular/fitness-professional/app.js
@@ -1,106 +1,108 @@
 'use strict';
 
-define(['app'], function(app) {
+define(['app'], function (app) {
 
 
-    app.register.controller("fitness-professionalCtrl",["$scope","$resource","rest","tokenError",
-        function($scope,$resource,tokenError){
+    app.register.controller("fitness-professionalCtrl", ["$scope", "$resource", "rest", "tokenError",
+        function ($scope, $resource, tokenError) {
 
             $scope.profession = [];
+            $scope.professionSelected = {};
             $scope.gender = [];
+            $scope.genderSelected = {};
             $scope.location = [];
             $scope.accepting = [];
             $scope.tagSelected = [];
             $scope.locations = [];
 
-            var professionalCollection =  $resource("http://:url/users/professionals",{
+            var professionalCollection = $resource("http://:url/users/professionals", {
                 url: $scope.restURL
             });
 
-            var filterProfessionalCollection =  $resource("http://:url/users/professionals?:filter",{
+            var filterProfessionalCollection = $resource("http://:url/users/professionals?:filter", {
                 url: $scope.restURL,
-                filter:'@filter'
+                filter: '@filter'
             });
 
-            var locationlCollection =  $resource("http://:url/users/location",{
+            var locationlCollection = $resource("http://:url/users/location", {
                 url: $scope.restURL
             });
 
-            $scope.professionals = professionalCollection.get(function() {},$scope.checkTokenError);
+            $scope.professionals = professionalCollection.get(function () {
+            }, $scope.checkTokenError);
 
-            $scope.locationsJson = locationlCollection.get(function() {
-                $scope.locationsJson.results.forEach(function(entry) {
+            $scope.locationsJson = locationlCollection.get(function () {
+                $scope.locationsJson.results.forEach(function (entry) {
                     $scope.locations.push(entry.location);
                 });
-            },$scope.checkTokenError);
+            }, $scope.checkTokenError);
+
 
 
             $scope.professionOnClick = function (value) {
-                if($scope.profession.indexOf(value) == -1){
+                if ($scope.profession.indexOf(value) == -1) {
                     $scope.profession.push(value);
+                    $scope.professionSelected[value] = true;
                 }
-                else{
-                    var temp = $scope.profession.indexOf(value);
-                    $scope.profession.splice(temp,1);
+                else {
+                    $scope.profession.splice($scope.profession.indexOf(value), 1);
+                    $scope.professionSelected[value] = false;
                 }
                 $scope.filter();
-            }
+            };
             $scope.genderOnClick = function (value) {
-               if($scope.gender.indexOf(value) == -1){
+                if ($scope.gender.indexOf(value) == -1) {
                     $scope.gender.push(value);
+                    $scope.genderSelected[value] = true;
                 }
-                else{
-                    var temp = $scope.gender.indexOf(value);
-                    $scope.gender.splice(temp,1);
+                else {
+                    $scope.gender.splice($scope.gender.indexOf(value), 1);
+                    $scope.genderSelected[value] = false;
+
                 }
                 $scope.filter();
-            }
+            };
             $scope.locationOnChange = function () {
-                if($scope.location.length <= 0){
+                if ($scope.location.length <= 0) {
                     $scope.location = [];
                 }
                 $scope.filter();
-            }
-            var tagCollection =  $resource("http://:url/tags/",{
+            };
+            var tagCollection = $resource("http://:url/tags/", {
                 url: $scope.restURL
             });
-            var tagResource = $resource("http://:url/tags/:id/",{
+            var tagResource = $resource("http://:url/tags/:id/", {
                 url: $scope.restURL,
-                id:'@id'
-            },{update: { method: 'PUT' }});
+                id: '@id'
+            }, {update: { method: 'PUT' }});
 
-            $scope.tags = tagCollection.get(function() {},$scope.checkTokenError);
-            $scope.addTag = function(tag) {
-            
+            $scope.tags = tagCollection.get(function () {
+            }, $scope.checkTokenError);
+            $scope.addTag = function (tag) {
+
                 // Ensures that no two tags are replicated
-                
-                if($scope.tagSelected.indexOf(tag) == -1){
-                        $scope.tagSelected.push(tag);
+
+                if ($scope.tagSelected.indexOf(tag) == -1) {
+                    $scope.tagSelected.push(tag);
                 }
-                else{
+                else {
                     var temp = $scope.tagSelected.indexOf(tag);
-                    $scope.tagSelected.splice(temp,1);
+                    $scope.tagSelected.splice(temp, 1);
                 }
                 $scope.filter()
 
-            }
+            };
             $scope.filter = function () {
                 $scope.filtering = {
                     profession: $scope.profession,
                     gender: $scope.gender,
                     location: $scope.location,
                     accepting: $scope.accepting,
-                    tags : $scope.tagSelected,
+                    tags: $scope.tagSelected,
                 };
-                $scope.professionals = filterProfessionalCollection.get($scope.filtering, function () {});
+                $scope.professionals = filterProfessionalCollection.get($scope.filtering, function () {
+                });
             };
-
-
-    }]);
-
-
-
-
-
-    return app;    
+        }]);
+    return app;
 });
