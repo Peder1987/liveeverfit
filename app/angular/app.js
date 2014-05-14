1841b9d18ccf63f1af5d7d6e457cdcc4fb15d78b
Nicolas Gonzalez <nickgzzjr@hotmail.com>
2014-05-14 00:14:44 -0500

    Messages page styled.

--- app/angular/app.js
+++ app/angular/app.js
@@ -35,7 +35,7 @@ define(['angularAMD', 'uiRouter', 'uiBootstrap', 'routeResolver', 'angularResour
 
                 //LoggedIN
                 .state('account-settings', route.resolve('/account-settings', 'account-settings'))
-                .state('dashboard', route.resolve('/dashboard', 'dashboard'))
+                .state('messages', route.resolve('/messages', 'messages'))
                 .state('workouts', route.resolve('/workouts', 'workouts'))
                 .state('workouts.video', route.resolve('/:id', 'workouts'))
                 .state('fitness-professional', route.resolve('/fitness-professional', 'fitness-professional'))
