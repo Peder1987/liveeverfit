916deaa232131ba7648b70137ea452d2e5f278a4
IsaaX <isaax@Bombero.(none)>
2014-05-15 14:12:05 -0500

    made first name and last name on dummy data

--- django/scripts/create_users_a_z.py
+++ django/scripts/create_users_a_z.py
@@ -13,22 +13,41 @@ import string
 from user_app.models import Professional
 
 def create_professional_dummy_data():
+	accepting = True
+	tags = ["looksy", 'crossfit', 'fitness']
+	counter = 0
 	for letter in string.ascii_uppercase:
 		email = 'pro_' + letter + '@test.com'
 		password = 'admin123'
+		first = 'pro_first_' + letter 
+		last = 'pro_last_' + letter
 
 		if not Professional.objects.filter(email=email).exists():
-			user = User.objects.create_user(email, password)
+			data_dict = {"email":email, "password" : password, "first_name":first, "last_name":last,
+				"password":password,
+			}
+			user = User.objects.create_user(**data_dict)
 			pro = Professional.objects.create_prof(user)
+			pro.is_accepting=accepting
+			pro.tags.add(tags[counter%3])
+			pro.save()
+		accepting = not accepting
+		counter += 1
 
+		
 
 def create_user_dummy_data():
 	for letter in string.ascii_uppercase:
 		email = 'user_' + letter + '@test.com'
 		password = 'admin123'
 
+		first = 'user_first_' + letter 
+		last = 'user_last_' + letter
 		if not User.objects.filter(email=email).exists():
-			user = User.objects.create_user(email, password)
+			data_dict = {"email":email, "password" : password, "first_name":first, "last_name":last,
+				"password":password,
+			}
+			user = User.objects.create_user(**data_dict)
 
 
 
--- django/test.py
+++ django/test.py
@@ -18,28 +18,9 @@ pros = Professional.objects.all()
 user =  User.objects.get(email='admin@test.com')
 pro = User.objects.get(email='pro2@test.com')
 
-print Professional.objects.filter(pk=user.id).exists()
-print Professional.objects.filter(pk=pro.id).exists()
+# print Professional.objects.filter(pk=user.id).exists()
+# print Professional.objects.filter(pk=pro.id).exists()
 import string
-def create_professional_dummy_data():
-	for letter in string.ascii_uppercase:
-		email = 'pro_' + letter + '@test.com'
-		password = 'admin123'
-
-		if not Professional.objects.filter(email=email).exists():
-			user = User.objects.create_user(email, password)
-			pro = Professional.objects.create_prof(user)
-
-
-def create_user_dummy_data():
-	for letter in string.ascii_uppercase:
-		email = 'user_' + letter + '@test.com'
-		password = 'admin123'
-
-		if not User.objects.filter(email=email).exists():
-			user = User.objects.create_user(email, password)
-
-
 
 
 create_professional_dummy_data()
