636e58c7e73b1b5d900b350f87a65e39f26fe95c
IsaaX <isaax@Bombero.(none)>
2014-05-16 14:36:22 -0500

    made more edits to professional script, added more data to db, finished pagination on fitness professionals

--- django/scripts/create_workout_videos.py
+++ django/scripts/create_workout_videos.py
@@ -11,3 +11,25 @@ import string
 ##############################################
 
 from user_app.models import Professional
+def create_workout_videos():
+	accepting = True
+	tags = ["looksy", 'crossfit', 'fitness']
+	profession_list = ['Nutritionist', 'Trainer']
+	counter = 0
+	for letter in string.ascii_uppercase:
+		email = 'pro_' + letter + '@test.com'
+		password = 'admin123'
+		pro = Professional.objects.get(email=email)
+		title = 'pro_title_' +letter
+		description = 'this is a description for video ' + title
+		difficulty = ['beginner', 'intermediate', 'advanced']
+
+		obj, created = Video.objects.get_or_create(user=pro, title=title,
+													description=description, difficulty=difficulty[(counter+1)%3])
+		
+		obj.video_tags.add(tags[counter%3])
+		
+		
+		
+		
+		counter += 1
--- django/test.py
+++ django/test.py
@@ -21,27 +21,3 @@ pro = User.objects.get(email='pro2@test.com')
 # print Professional.objects.filter(pk=user.id).exists()
 # print Professional.objects.filter(pk=pro.id).exists()
 import string
-def create_workout_videos():
-	accepting = True
-	tags = ["looksy", 'crossfit', 'fitness']
-	profession_list = ['Nutritionist', 'Trainer']
-	counter = 0
-	for letter in string.ascii_uppercase:
-		email = 'pro_' + letter + '@test.com'
-		password = 'admin123'
-		pro = Professional.objects.get(email=email)
-		title = 'pro_title_' +letter
-		description = 'this is a description for video ' + title
-		difficulty = ['beginner', 'intermediate', 'advanced']
-
-		obj, created = Video.objects.get_or_create(user=pro, title=title,
-													description=description, difficulty=difficulty[(counter+1)%3])
-		
-		obj.video_tags.add(tags[counter%3])
-		
-		
-		
-		
-		counter += 1
-
-create_workout_videos()
