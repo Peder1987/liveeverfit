46c44207c4a0e716deb9a66ddb69928557731d9b
Miguel <miguel@miguel-Inspiron.(none)>
2014-05-20 14:14:08 -0500

    change path of media in django

--- django/django_server/settings.py
+++ django/django_server/settings.py
@@ -9,7 +9,7 @@ DJANGO_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)
 PROJECT_PATH = os.path.abspath(os.path.join(DJANGO_PATH, os.pardir))
 
 # Project path contains media and angular path
-MEDIA_PATH = os.path.join(PROJECT_PATH, 'app/angular/common/media')
+MEDIA_PATH = os.path.join(PROJECT_PATH, 'app/angular/media')
 
 # Un-Comment this for UserApp
 AUTH_USER_MODEL = 'user_app.CustomUser'
@@ -69,7 +69,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
+MEDIA_ROOT = MEDIA_PATH
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
