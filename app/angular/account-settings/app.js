7957f999bb7d56ab3aed6f94b0db5837a0036158
Miguel <miguel@miguel-Inspiron.(none)>
2014-05-19 20:25:40 -0500

    working factory for share picture, ready for upload

--- app/angular/account-settings/app.js
+++ app/angular/account-settings/app.js
@@ -419,70 +419,68 @@ define(['app'], function(app) {
 
 
 
-    //This is Jcrop
-    app.register.factory('shareImg', ['$rootScope', function ($rootScope) {
-		var data = {};
-		return data;
-    }]);
-
-
-    app.register.factory('fileReader', 
-        function($q){
-            var onLoad = function (reader, deferred, scope) {
-                return function () {
-                    scope.$apply(function () {
-                        deferred.resolve(reader.result);
-                    });
-                };
-            };
-            var onError = function (reader, deferred, scope) {
-                return function () {
-                    scope.$apply(function () {
-                        deferred.reject(reader.result);
-                    });
-                };
-            };
-            var onProgress = function (reader, scope) {
-                return function (event) {
-                    scope.$broadcast(
-                        "fileProgress", 
-                        { total: event.total,
-                            loaded: event.loaded
-                        });
-                };
-            };
-            var getReader = function (deferred, scope) {
-                var reader = new FileReader();
-                reader.onload = onLoad(reader, deferred, scope);
-                reader.onerror = onError(reader, deferred, scope);
-                reader.onprogress = onProgress(reader, scope);
-                return reader;
-            };
-            var readAsDataURL = function (file, scope) {
-                var deferred = $q.defer();
-                var reader = getReader(deferred, scope);
-                reader.readAsDataURL(file);
-                return deferred.promise;
-            };
-            return { readAsDataUrl: readAsDataURL };
+    //JCrop
+    app.register.factory("shareImg", function($rootScope){
+    	var data = {};
+    	return data;
     });
 
 
-	app.register.directive('fileselect', [
-        function(){
-            return {
-                link: function(scope, element, attributes) {
-                    element.bind("change", function(changeEvent) {
-                        scope.img = changeEvent.target.files[0];
-                        scope.getFile();
-                    });
-                }
+    app.register.factory("fileReader", function($q){
+    	var onLoad = function (reader, deferred, scope) {
+    		return function () {
+                scope.$apply(function () {
+                    deferred.resolve(reader.result);
+                });
             };
-    }]);
+        };
+        var onError = function (reader, deferred, scope) {
+            return function () {
+                scope.$apply(function () {
+                    deferred.reject(reader.result);
+                });
+            };
+        };
+        var onProgress = function (reader, scope) {
+            return function (event) {
+                scope.$broadcast(
+                	"fileProgress", 
+                	{ total: event.total,
+                		loaded: event.loaded
+                	});
+            };
+        };
+        var getReader = function (deferred, scope) {
+            var reader = new FileReader();
+            reader.onload = onLoad(reader, deferred, scope);
+            reader.onerror = onError(reader, deferred, scope);
+            reader.onprogress = onProgress(reader, scope);
+            return reader;
+        };
+        var readAsDataURL = function (file, scope) {
+            var deferred = $q.defer();
+            var reader = getReader(deferred, scope);
+            reader.readAsDataURL(file);
+            return deferred.promise;
+        };
+        return { readAsDataUrl: readAsDataURL };
+    });
 
 
-    app.register.directive('imgCropped', ['$window','shareImg',
-        function($window,shareImg) {
+	app.register.directive("fileselect", function(){
+        return {
+            link: function(scope, element, attributes) {
+                element.bind("change", function(changeEvent) {
+                    scope.img = changeEvent.target.files[0];
+                    scope.getFile();
+                });
+            }
+        };
+    });
+
+
+    app.register.directive("imgCropped", ["$window","shareImg",
+    	function($window,shareImg) {
 
             var bounds = {};
             return {
@@ -502,10 +500,8 @@ define(['app'], function(app) {
 
                 scope.$watch('src', function (nv) {
                     clear();
-                    // console.log('[src]');
-                    // console.log(nv);
-                    if (!nv) { // newValue
-                        return;
+                    if (!nv){
+                    	return;
                     }
                     element.after('<img style="max-width: 100%;"/>');
                     scope.$parent.myImg = element.next();
@@ -525,18 +521,17 @@ define(['app'], function(app) {
                         var boundsArr = this.getBounds();
                         bounds.x = boundsArr[0];
                         bounds.y = boundsArr[1];
-                        //Shared Factory
-                        shareImg.imgOriginal = scope.$parent.img;
+                        //Factory
+                        shareImg.imgOrig = scope.$parent.img;
                     });
                 });
                 scope.$on('$destroy', clear);
               }
             };
-
     }]);
 
 
-    app.register.controller('ProfilePicCtrl', ['$window', '$timeout', '$scope', 'fileReader','shareImg',
+    app.register.controller('ProfilePicCtrl', ["$window","$timeout","$scope","fileReader","shareImg",
         function($window, $timeout, $scope, fileReader,shareImg) {
 
             $scope.getFile = function(){
@@ -554,15 +549,10 @@ define(['app'], function(app) {
             });
 
             $scope.initJcrop = function(){
-                // console.log('init jcrop');
                 $window.jQuery('img.aj-crop').Jcrop({
                     onSelect: function(){
-                        //$scope.$apply();
-                        // console.log('onSelect', arguments);
                     }, 
                     onChange: function(){
-                        //$scope.$apply();
-                        // console.log('onChange', arguments);
                     }, 
                     trackDocument: true, 
                     aspectRatio: 1.333333333333333333
@@ -574,43 +564,32 @@ define(['app'], function(app) {
               $scope.picWidth = cords.w;
               $scope.picHeight = cords.h;
 
-              // console.log('scale');
               if ($scope.picWidth > 400) {
                 scale = (400 / $scope.picWidth);
-                // console.log($scope.picHeight);
                 $scope.picHeight *= scale;
                 $scope.picWidth *= scale;
-                // console.log(scale);
               }
-
               if ($scope.picHeight > 400) {
                 scale = (400 / $scope.picHeight);
                 $scope.picHeight *= scale;
                 $scope.picWidth *= scale;
-                // console.log(scale);
               }
-
-              // console.log('[cords]', $scope.picWidth / $scope.picHeight);
-              // console.log(cords);
               $scope.cropped = true;
-
               var rx = $scope.picWidth / cords.w, 
-                    ry = $scope.picHeight / cords.h, 
+              		ry = $scope.picHeight / cords.h, 
                     canvas = document.createElement("canvas"), 
                     context = canvas.getContext('2d'), 
                     imageObj = $window.jQuery('img#preview')[0];
-
               $window.jQuery('img#preview').css({
-                width: Math.round(rx * cords.bx) + 'px',
+              	width: Math.round(rx * cords.bx) + 'px',
                 height: Math.round(ry * cords.by) + 'px',
                 marginLeft: '-' + Math.round(rx * cords.x) + 'px',
                 marginTop: '-' + Math.round(ry * cords.y) + 'px'
               });
-
             };
 
     }]);
-	//This is the end of Jcrop
+	//End JCrop
 
 
  	
