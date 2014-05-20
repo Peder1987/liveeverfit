5467cf822edc0d1f3824bc0c4c5d5f302e4e134e
IsaaX <isaax@Bombero.(none)>
2014-05-20 01:10:46 -0500

    Finished deletion on msgs, made sure it reflected wheather user deleted from inbox or sent

--- django/messages/serializers.py
+++ django/messages/serializers.py
@@ -7,6 +7,7 @@ from django.utils.timezone import now
 
 class InboxSerializer(serializers.ModelSerializer):
     sender = serializers.IntegerField(source='sender.email', required=True)  
+    recipient = serializers.IntegerField(source='recipient.email', required=True)  
     img = serializers.IntegerField(source='sender.img.url', required=True)  
     #recipient = serializers.CharField(source='recipient.email', required=False)
     
@@ -14,26 +15,35 @@ class InboxSerializer(serializers.ModelSerializer):
         model = Message
     
     
-class SentSerializer(serializers.ModelSerializer):
-    recipient = serializers.IntegerField(source='recipient.email', required=True)  
-    img = serializers.IntegerField(source='recipient.img.url', required=True)  
-    class Meta:
-        model = Message
+class SentSerializer(InboxSerializer):
+    pass
         
+class TrashSerializer(InboxSerializer):
+    pass
 
 class DeleteSerializer(serializers.ModelSerializer):
+    sender = serializers.SlugRelatedField(slug_field="email", required=False)  
+    recipient = serializers.SlugRelatedField(slug_field="email", required=False)  
+    view = serializers.CharField(required=True)  
     class Meta:
         model = Message
-        fields = ('id',)
+        fields = ('id', 'recipient', 'sender', 'view')
+        exclude = ('img', 'body', 'subject')
+
     def restore_object(self, attrs, instance=None):
         """
         Given a dictionary of deserialized field values, either update
         an existing model instance, or create a new model instance.
         """
         obj = super(DeleteSerializer, self).restore_object(attrs, instance)
+        
+        if 'inbox' in attrs['view']:
+            obj.recipient_deleted_at = now()
+            obj.save()
+        if 'sent' in attrs['view']:
+            obj.sender_deleted_at = now()
+            obj.save()
 
-        obj.recipient_deleted_at = now()
-        obj.save()
         return obj
 
 
@@ -41,6 +51,13 @@ class UnDeleteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ('id',)
+    def __init__(self, *args, **kwargs):
+        
+        # Logged in user to be sender
+        user = kwargs['context']['request'].user
+        kwargs['data']['sender'] = user.pk
+        return super(ComposeSerializer, self).__init__(*args, **kwargs)
+
     def restore_object(self, attrs, instance=None):
         """
         Given a dictionary of deserialized field values, either update
