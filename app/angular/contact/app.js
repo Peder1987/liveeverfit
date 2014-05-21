e0342fae61ae3b9b69b7bc5a7d60bb1cdbc7a670
joseluisquintanat <borre.quintana@gmail.com>
2014-05-21 18:36:46 -0500

    evergreen

--- app/angular/contact/app.js
+++ app/angular/contact/app.js
@@ -5,5 +5,12 @@ define(['app'], function(app) {
     app.register.controller('contactCtrl', ['$scope',
     	function($scope) {
     }]);
+
+    app.register.controller('contactCtrl', ['$scope',
+        function($scope) {
+            $scope.name = "";
+            $scope.email = "";
+            $scope.message = "";
+    }]);
     
 });
