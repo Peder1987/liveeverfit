5bd8c86bae49c3181be4f2c4bab48a84d00975d2
Miguel <miguel@miguel-Inspiron.(none)>
2014-05-21 19:16:28 -0500

    contact us almost doe in front end

--- app/angular/contact/app.js
+++ app/angular/contact/app.js
@@ -6,11 +6,20 @@ define(['app'], function(app) {
     	function($scope) {
     }]);
 
-    app.register.controller('contactCtrl', ['$scope',
-        function($scope) {
+    app.register.controller('contactCtrl', ['$scope','$state',
+        function($scope,$state) {
             $scope.name = "";
             $scope.email = "";
             $scope.message = "";
+
+            $scope.submit = function(valid){
+            	if(valid == true){
+            		console.log($scope.name);
+            		console.log($scope.email);
+            		console.log($scope.message);
+            		// $state.go('home');
+            	}
+            };
     }]);
     
 });
