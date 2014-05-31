1ce3af0292678c2ed83dd22591e32851f0f135fe
Miguel Vazquez <migueldv90@yahoo.com>
2014-05-31 16:46:41 -0500

    calendar working now i have to fix the little bugs

---
+++ django/schedule/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from rest_framework import routers
+from schedule.models import Calendar
+from schedule.views import EventViewSet,EventObjectViewSet, EventViewSet
+
+
+
+# router = routers.SimpleRouter(trailing_slash=False)
+# router.register('', CalendarViewSet)
+# router.register('event', EventViewSet)
+
+
+urlpatterns = patterns('',
+    url(r'^(?P<pk>[0-9]+)$', EventViewSet.as_view()),
+    url(r'^event/?$', EventViewSet.as_view()),
+    url(r'^event/(?P<pk>[0-9]+)$', EventObjectViewSet.as_view()),
+)
