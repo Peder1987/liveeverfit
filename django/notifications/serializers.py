ef60f459af5bfb6481a9807fd19ef5c07722eda6
Miguel Vazquez <migueldv90@yahoo.com>
2014-06-08 17:05:08 -0500

    notifications done with our stylying and heartbeat

--- django/notifications/serializers.py
+++ django/notifications/serializers.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import Group, Permission
 from django.contrib.auth import get_user_model
 User = get_user_model()
 from rest_framework import serializers
-
 from .models import Notification
+
+
 
 class AllNotificationSerializer(serializers.ModelSerializer):
 	message = serializers.Field(source="message")
--- django/notifications/views.py
+++ django/notifications/views.py
@@ -6,76 +6,12 @@ from django.template.context import RequestContext
 from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework import generics
-
+from rest_framework.response import Response
 
 from .models import Notification
 from .permissions import IsAdminOrSelf
 from .serializers import AllNotificationSerializer
-@login_required
-def all(request):
-    """
-    Index page for authenticated user
-    """
-    return render(request, 'notifications/list.html', {
-        'notifications': request.user.notifications.all()
-    })
-    actions = request.user.notifications.all()
 
-    paginator = Paginator(actions, 16) # Show 16 notifications per page
-    page = request.GET.get('p')
-
-    try:
-        action_list = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        action_list = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        action_list = paginator.page(paginator.num_pages)
-        
-    return render_to_response('notifications/list.html', {
-        'member': request.user,
-        'action_list': action_list,
-    }, context_instance=RequestContext(request))
-
-@login_required
-def unread(request):
-    return render(request, 'notifications/list.html', {
-        'notifications': request.user.notifications.unread()
-    })
-    
-@login_required
-def mark_all_as_read(request):
-    request.user.notifications.mark_all_as_read()
-    return redirect('notifications:all')
-
-@login_required
-def mark_as_read(request, slug=None):
-    id = slug2id(slug)
-
-    notification = get_object_or_404(Notification, recipient=request.user, id=id)
-    notification.mark_as_read()
-
-    next = request.REQUEST.get('next')
-
-    if next:
-        return redirect(next)
-
-    return redirect('notifications:all')
-
-@login_required
-def mark_as_unread(request, slug=None):
-    id = slug2id(slug)
-
-    notification = get_object_or_404(Notification, recipient=request.user, id=id)
-    notification.mark_as_unread()
-
-    next = request.REQUEST.get('next')
-
-    if next:
-        return redirect(next)
-
-    return redirect('notifications:all')
 
 
 class AllNotificationViewSet(viewsets.ModelViewSet):
@@ -84,32 +20,14 @@ class AllNotificationViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAdminOrSelf,)
 
     def get_queryset(self):
-        return self.request.user.notifications.all()
+        return self.request.user.notifications.filter(unread = True)
 
-
-    def create(self, request):
-        """
-        Removes post functionality and marks all notifications and read,
-        can individually update notifications if owner
-        """
-        request.user.notifications.mark_all_as_read()
-        return Response({'status': 'notifications read'})
+    def update(self, request, pk=None):
+        if Notification.objects.filter(pk = pk).exists():
+            notification =  Notification.objects.get(pk = pk)
+            notification.unread = False
+            notification.save()
+        else:
+            pass
+        return Response({'details':['success']}, status=status.HTTP_200_OK)
         
-
-
-            
-# unread
-# mark_all_as_read
-# mark_as_read
-# mark_as_unread
-# class UnreadNotificationView(generics.UpdateAPIView):
-#     model = Notification
-#     permission_classes = (IsAdminOrSelf,)
-#     def get_queryset(self):
-#         return self.request.user.notifications.all()
-
-# class MarkAllReadNotificationView(generics.UpdateAPIView):
-#     model = Notification
-#     permission_classes = (IsAdminOrSelf,)
-#     def get_queryset(self):
-#         return self.request.user.notifications.all()
