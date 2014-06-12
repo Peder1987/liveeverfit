1ef75073336b22b99bf4d7893628aaf5c03e60a4
Nicolas Gonzalez <nickgzzjr@hotmail.com>
2014-06-12 12:58:36 -0500

    IE Bug Profile Picture fixed.

--- app/angular/app.js
+++ app/angular/app.js
@@ -108,7 +108,7 @@ define(['angularAMD',
 
         app.service('rest', ['$rootScope', function ($rootScope) {
             $rootScope.restProtocol = "http";
-            $rootScope.restURL = "localhost:8000";
+            $rootScope.restURL = "192.168.1.100:8000";
         }]);
         app.service('restricted', ['$rootScope', 'localStorageService', function ($rootScope, localStorageService) {
             $rootScope.restricted = function () {
