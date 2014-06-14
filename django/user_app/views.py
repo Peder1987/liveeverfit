dc8fd54d99a6865b7668b2634c9fe5ec556af145
Isaac Duran <isaax@utexas.edu>
2014-06-14 16:32:17 -0500

    keep pagination and return whole table instead

--- django/user_app/views.py
+++ django/user_app/views.py
@@ -146,7 +146,7 @@ class GroupTagView(generics.ListAPIView):
         else:
             return []
 class StaticTagViewSet(generics.ListAPIView):
-    paginate_by = None
+    paginate_by = 50
     model = StaticTags
     permission_classes = (AllowAny,)
     serializer_class = StaticTagSerializer
