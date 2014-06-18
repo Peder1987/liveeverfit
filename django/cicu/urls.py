0f7c6ee6c0dd65477122b3e221fea1dad8bc99a7
Isaac Duran <isaax@utexas.edu>
2014-06-17 20:15:52 -0500

    readying up for online testing

--- django/cicu/urls.py
+++ django/cicu/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('cicu.views',
     url(r'^$', 'upload', name='upload-image'),
-    url(r'^crop/$', 'crop', name='image-crop'),
-    url(r'^upload-profile-picture/$', 'upload_profile_picture', name='image-upload'),
-    url(r'^crop-profile-picture/$', 'crop_profile_picture', name='image-crop'),
+    url(r'^crop$', 'crop', name='image-crop'),
+    url(r'^upload-profile-picture$', 'upload_profile_picture', name='image-upload'),
+    url(r'^crop-profile-picture$', 'crop_profile_picture', name='image-crop'),
 )
