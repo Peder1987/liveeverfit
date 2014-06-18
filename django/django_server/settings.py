e99196a4c94ac6388d8d0ee6cb7232378bf7c8f4
Isaac Duran <isaax@utexas.edu>
2014-06-17 20:43:00 -0500

    trying another combo

--- django/django_server/settings.py
+++ django/django_server/settings.py
@@ -195,8 +195,8 @@ INSTALLED_APPS = (
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_COOKIE_DOMAIN = ".liveeverfit.com"
-SESSION_COOKIE_DOMAIN = ".liveeverfit.com"
+CSRF_COOKIE_DOMAIN = ".dev.liveeverfit.com"
+SESSION_COOKIE_DOMAIN = ".dev.liveeverfit.com"
 # MISC APP VARIABLES
 TOKEN_EXPIRE = True
 # Defaults to 14
