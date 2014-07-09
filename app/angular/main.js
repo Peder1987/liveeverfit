fa00cccf5185570a2e7fc8a600bd21d5f19b09a8
Nicolas Gonzalez <nickgzzjr@hotmail.com>
2014-07-09 13:20:26 -0500

    version bump

--- app/angular/main.js
+++ app/angular/main.js
@@ -117,7 +117,7 @@ require.config({
         'mm.foundation' : ['angular']
     },
     // Version the app to avoid cache issues
-    urlArgs: "0.0.19",
+    urlArgs: "0.0.20",
     // Kick start application
     deps: ['app']
 });
