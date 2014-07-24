7754c9159b166da13d75ea18b554b0c0b797c9ae
Isaac Duran <isaax@utexas.edu>
2014-07-24 11:24:27 -0500

    added rule for tags

--- django/taggit/admin.py
+++ django/taggit/admin.py
@@ -17,5 +17,12 @@ class TagAdmin(admin.ModelAdmin):
     search_fields = ["name"]
     prepopulated_fields = {"slug": ["name"]}
 
+    def save_model(self, request, obj, form, change):
+    	# in order to prevent same slugs and complications
+    	# rule will be all tags must be lowercase
+    	obj.name = obj.name.lower()
+    	super(TagAdmin, self).save_model(request, obj, form, change)
+
+
 
 admin.site.register(Tag, TagAdmin)
