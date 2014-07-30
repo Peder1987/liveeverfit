13c590fbcc88d70c80129c5b343ab92b60d10520
Isaac Duran <isaax@utexas.edu>
2014-07-30 11:36:16 -0500

    added script for shopify id lookup

--- django/test.py
+++ django/test.py
@@ -27,6 +27,14 @@ from feed.models import Entry, TextEntry, PhotoEntry
 
 from schedule.models import Calendar
 
+from shopify_app import shopify_call
+users = User.objects.all()
 
-for user in User.objects.all():
-	Calendar.objects.get_or_create(user=user)
+for user in users:
+	try:
+		print user.email
+		user.shopify_id = shopify_call.requests_search(user.email)['id']
+		user.save()
+		print user.shopify_id
+	except:
+		print 'doesnt exists'
