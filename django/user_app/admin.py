defb8c95627a2672adeadf5d2a387eee87b20c02
Isaac Duran <isaax@utexas.edu>
2014-08-07 12:15:07 -0500

    emails professionals only if they get accepted in admin panel, also added a decline function that changes pro to tier 1 if they arenot accepted

--- django/user_app/admin.py
+++ django/user_app/admin.py
@@ -1,14 +1,14 @@
 import csv
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.template import RequestContext
+from django.template import RequestContext, Context
 from django.template.loader import get_template
 from django.http import HttpResponse
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import models as auth_models
 from django.utils.safestring import mark_safe
 from django.core.urlresolvers import reverse
-from django.core.mail import send_mail, EmailMessage
+from django.core.mail import send_mail, EmailMessage, EmailMultiAlternatives
 
 from user_app.models import CustomUser, Address, Professional, UniqueLocation, Certification, FeaturedProfessional, StaticTags
 from user_app.forms import CustomUserChangeForm, CustomUserCreationForm
@@ -104,14 +104,58 @@ def export_as_csv(modeladmin, request, queryset):
 
 def make_active(modeladmin, request, queryset):
     for obj in queryset:
+        email = 'payroll@liveeverfit.com'
+        subject = 'New Professional'
+        message = 'New Professional in Live Ever Fit \n' + 'Name: ' + obj.first_name + ' ' + obj.last_name + '\n' + 'Email: ' + obj.email + '\n' + 'phone: ' + obj.phone + '\n' + 'address: ' + obj.primary_address.street_line1 + ' ' + obj.primary_address.street_line2 + ' ' + obj.primary_address.city + ' ' + obj.primary_address.state + ' ' + obj.primary_address.zipcode   
+        send_mail(subject, message, 'info@liveeverfit.com', [email])
+        if obj.profession == 'Nutritionist':
+            email = obj.email
+            subject = 'Welcome to Live Ever Fit'
+            message = 'Thank you for registering to Live Ever Fit'
+            template = get_template('email/nutritionist.html')
+            context = Context({})
+            output = template.render(context)
+            msg = EmailMultiAlternatives(subject, message, 'info@liveeverfit.com', [email])
+            msg.attach_alternative(output, "text/html")
+            msg.send()
+        elif obj.profession == 'Trainer':
+            email = obj.email
+            subject = 'Welcome to Live Ever Fit'
+            message = 'Thank you for registering to Live Ever Fit'
+            template = get_template('email/trainer.html')
+            context = Context({})
+            output = template.render(context)
+            msg = EmailMultiAlternatives(subject, message, 'info@liveeverfit.com', [email])
+            msg.attach_alternative(output, "text/html")
+            msg.send()
+        elif obj.profession == 'Promoter':
+            email = obj.email
+            subject = 'Welcome to Live Ever Fit'
+            message = 'Thank you for registering to Live Ever Fit'
+            template = get_template('email/promoter.html')
+            context = Context({})
+            output = template.render(context)
+            msg = EmailMultiAlternatives(subject, message, 'info@liveeverfit.com', [email])
+            msg.attach_alternative(output, "text/html")
+            msg.send()
+        elif obj.profession == 'Instructor':
+            print 'No Email Template'
         obj.queue = False
         obj.save()
         
 
 def make_inactive(modeladmin, request, queryset):
+    # makes the professional not appear on list
     for obj in queryset:
         obj.queue = True
         obj.save()
+
+def decline_professional_move_to_tier_1(modeladmin, request, queryset):
+    #makes a professional to a tier 1, no specifications on how they want it 
+    for obj in queryset:
+        obj.delete_professional()
+
+
 
 class ProfessionalAdmin(UserAdmin):
     # The forms to add and change user instances
@@ -141,7 +185,7 @@ class ProfessionalAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'queue')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
-    actions = [make_active, make_inactive, export_as_csv] 
+    actions = [make_active, make_inactive, decline_professional_move_to_tier_1, export_as_csv] 
     inlines = [CertificationInline,]
 
 admin.site.register(Professional, ProfessionalAdmin)
