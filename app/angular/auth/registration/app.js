9f4522a1071eaeb45db63cacffdb24f5a04cac63
Nicholas Thompson <ubergeek.2.0a@gmail.com>
2014-08-20 21:59:00 -0500

    Katies changes made

--- app/angular/auth/registration/app.js
+++ app/angular/auth/registration/app.js
@@ -76,6 +76,17 @@ define(['app'], function(app) {
 				address_state : "",
 				address_zip : "",
     		};
+    		$scope.choices = [{"id":1, "value":"1", "label":"Good"}, {"id":2, "value":"2","label":"Ok"},{"id":3, "value":"3","label":"Bad"}];
+    $scope.value = [];
+    $scope.updateQuestionValue = function(choice){
+        $scope.value = $scope.value || [];
+        if(choice.checked){
+            $scope.value.push(choice.value);
+            $scope.value = _.uniq($scope.value);
+        }else{
+            $scope.value = _.without($scope.value, choice.value);
+        }
+    };
 
 
     		var AuthToken =  $resource(":protocol://:url/accounts/register/", {
